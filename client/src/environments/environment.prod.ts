d395e3053ebb2a6f4f0f9a104dd965780170de4c
Varanasi Shanti Suma <Varanasi.Shanti.Suma@ibm.com>
2026-03-23 14:58:21 +0530

    Updating urls

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: ' https://pulse-ops-p2xq.onrender.com/api',
+  apiUrl: 'https://pulse-ops-p2xq.onrender.com/api',
   wsUrl:  'wss:https://pulse-ops-p2xq.onrender.com',
 };
