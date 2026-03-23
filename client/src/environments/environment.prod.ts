edef22ac2c5d60882d84279fd42ac82ad011d49c
Varanasi Shanti Suma <Varanasi.Shanti.Suma@ibm.com>
2026-03-23 15:03:36 +0530

    Fix WebSocket URL in production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pulse-ops-p2xq.onrender.com/api',
-  wsUrl:  'wss:https://pulse-ops-p2xq.onrender.com',
+  apiUrl: 'https://pulse-ops.onrender.com/api',
+  wsUrl:  'wss://pulse-ops.onrender.com',
 };
